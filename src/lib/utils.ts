22cd614612aae94c909bf776dcbf2132117a93a3
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2024-08-07 11:09:07 -0500

    change backend route

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const API_URL = "http://ec2-34-200-212-171.compute-1.amazonaws.com/api/users";
+export const API_URL = "http://ec2-44-195-45-59.compute-1.amazonaws.com/api/users";
