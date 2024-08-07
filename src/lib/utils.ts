6c46191a266353fe6da490b636e4b3f1f969d5ec
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2024-08-07 11:10:15 -0500

    xd

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,4 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const API_URL = "http://ec2-44-195-45-59.compute-1.amazonaws.com/api/users";
+export const API_URL =
+  "http://ec2-44-195-45-59.compute-1.amazonaws.com/api/users";
